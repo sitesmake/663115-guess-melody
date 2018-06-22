b31dcf8fb2b067bb77bd54c0ea74e43afb5c3ed3
Alexander <sitesmake@gmail.com>
2018-06-22 19:11:13 +0300

    Add missed screen

--- js/screens-controller.js
+++ js/screens-controller.js
@@ -2,12 +2,14 @@ import welcomeScreen from './screens/welcome-controller';
 import genreScreen from './screens/genre-controller';
 import artistScreen from './screens/artist-controller';
 import resultScreen from './screens/result-controller';
+import looseLivesScreen from './screens/loose-lives-controller';
 
 const screens = {
   'welcome': welcomeScreen,
   'genre': genreScreen,
   'artist': artistScreen,
-  'result': resultScreen
+  'result': resultScreen,
+  'loose-lives': looseLivesScreen
 };
 
 const mainElement = document.querySelector(`.main`);
---
+++ js/screens/loose-lives-controller.js
@@ -0,0 +1,5 @@
+import LooseLivesView from '../views/loose-lives-view';
+
+const looseLivesScreen = new LooseLivesView();
+
+export default looseLivesScreen;
